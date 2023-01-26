347deab88bc7eb7a415dcab6888dcd83858fa3b3
Joe Chesmore <joe.chesmore89@gmail.com>
2023-01-25 23:46:10 -0500

    Initial Commit

---
+++ DiceGames/DiceGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceGamesApp.swift
+//  DiceGames
+//
+//  Created by Joseph CHESMORE on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct DiceGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
